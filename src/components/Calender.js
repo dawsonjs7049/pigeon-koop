0d8ca8b1c09b07ff5b1498c73d5ea3a4915480f4
JakeTheCodingSnake <jake906@charter.net>
2023-06-11 17:22:14 -0500

    who knows what the heck im doing

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,4 +1,3 @@
-import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
@@ -6,6 +5,7 @@ import { useRef, useState } from 'react';
 import { getDateRange, formatDate } from '@/utils/utilities';
 import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, ModalOverlay, Modal, Text, Button, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, HStack, useToast } from '@chakra-ui/react';
 import { addDoc, collection, deleteDoc, doc, serverTimestamp, Timestamp, updateDoc } from 'firebase/firestore';
+import FullCalendar from '@fullcalendar/react';
 
 export default function Calendar({ events, user, db, name }) {
 
