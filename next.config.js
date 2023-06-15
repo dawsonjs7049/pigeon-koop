caf449bf498c353fe68478281d64cb4c01069455
JakeTheCodingSnake <jake906@charter.net>
2023-06-14 19:22:36 -0500

    revert the transpile stuff and update dependencies

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// }
-
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/core',
-  '@fullcalendar/google-calendar',
-  '@fullcalendar/interaction',
-]);
-
-module.exports = withTM({ reactStrictMode: true });
+module.exports = nextConfig
--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,5 +1,4 @@
-'use client'
-
+import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
@@ -7,7 +6,6 @@ import { useRef, useState } from 'react';
 import { getDateRange, formatDate } from '@/utils/utilities';
 import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, ModalOverlay, Modal, Text, Button, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, HStack, useToast } from '@chakra-ui/react';
 import { addDoc, collection, deleteDoc, doc, serverTimestamp, Timestamp, updateDoc } from 'firebase/firestore';
-import FullCalendar from '@fullcalendar/react';
 
 export default function Calendar({ events, user, db, name }) {
 
